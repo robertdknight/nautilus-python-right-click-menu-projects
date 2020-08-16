9a5de88cf47504b4e54d57f5b980e51bccee3be7
Robert Knight <26532630+robertdknight@users.noreply.github.com>
2020-08-15 22:57:41 -0500

    Update text-editor-choice.py
    Added Kate and Gnome Builder

--- text-editor-choice.py
+++ text-editor-choice.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# Copyright (c) 2017 Robert Knight
+# Copyright (c) 2017-2020  Robert Knight
+# All rights reserved.
 
 # This creates a menu item that says "Open File With"
 # and then, the two programs are options to open the file with.  In this particular example
@@ -12,6 +13,9 @@ from urllib import unquote
 PROGRAM_NAME = 'code'
 PROGRAM_NAME2 = 'gedit'
 PROGRAM_NAME3 = 'mousepad'
+PROGRAM_NAME4 = 'gnome-builder'
+PROGRAM_NAME5 = 'kate'
+
 
 class ExampleMenuProvider(GObject.GObject, Nautilus.MenuProvider):
     def __init__(self):
@@ -39,6 +43,22 @@ class ExampleMenuProvider(GObject.GObject, Nautilus.MenuProvider):
         file_name = [unquote(item.get_uri()[7:]) for item in files]
         argv = [PROGRAM_NAME3] + file_name
         GObject.spawn_async(argv, flags=GObject.SPAWN_SEARCH_PATH)
+    
+    def launch_program4(self, menu, files):
+        if len(files) == 0:
+            return
+        # Strip the URI format to plain file names
+        file_name = [unquote(item.get_uri()[7:]) for item in files]
+        argv = [PROGRAM_NAME4] + file_name
+        GObject.spawn_async(argv, flags=GObject.SPAWN_SEARCH_PATH)
+        
+    def launch_program5(self, menu, files):
+        if len(files) == 0:
+            return
+        # Strip the URI format to plain file names
+        file_name = [unquote(item.get_uri()[7:]) for item in files]
+        argv = [PROGRAM_NAME5] + file_name
+        GObject.spawn_async(argv, flags=GObject.SPAWN_SEARCH_PATH)
 
     def get_file_items(self, window, files):
 
@@ -49,22 +69,22 @@ class ExampleMenuProvider(GObject.GObject, Nautilus.MenuProvider):
                                          icon='')
 
         #  Submenu items when a file is selected -----------------
+        
         submenu = Nautilus.Menu()
         top_menuitem.set_submenu(submenu)
 
         sub_menuitem = Nautilus.MenuItem(name='ExampleMenuProvider::Code',
-                                         label='Code',
+                                         label='VS Code',
                                          tip='',
                                          icon='')
         submenu.append_item(sub_menuitem)
         sub_menuitem.connect('activate', self.launch_program1, files)
 
-        sub_menuitem2 = Nautilus.MenuItem(name='ExampleMenuProvider::Gedit',
+        sub_menuitem2 = Nautilus.MenuItem(name='ExampleMenuProvider::GEdit',
                                          label='Gedit',
                                          tip='',
                                          icon='')
         submenu.append_item(sub_menuitem2)
-
         sub_menuitem2.connect('activate', self.launch_program2, files)
 
         sub_menuitem3 = Nautilus.MenuItem(name='ExampleMenuProvider::Mousepad',
@@ -72,8 +92,20 @@ class ExampleMenuProvider(GObject.GObject, Nautilus.MenuProvider):
                                          tip='',
                                          icon='')
         submenu.append_item(sub_menuitem3)
-
         sub_menuitem3.connect('activate', self.launch_program3, files)
+        
+        sub_menuitem4 = Nautilus.MenuItem(name='ExampleMenuProvider::Builder',
+                                         label='Builder',
+                                         tip='',
+                                         icon='')
+        submenu.append_item(sub_menuitem4)
+        sub_menuitem4.connect('activate', self.launch_program4, files)
 
+        sub_menuitem5 = Nautilus.MenuItem(name='ExampleMenuProvider::Kate',
+                                         label='Kate',
+                                         tip='',
+                                         icon='')
+        submenu.append_item(sub_menuitem5)
+        sub_menuitem5.connect('activate', self.launch_program5, files)
+        
         return top_menuitem,
-
